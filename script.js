6b2cbbd3d648011aa121846c313b8fc84512e931
Dima Tsymbal <dima.tsymbal12345@gmail.com>
2021-11-01 12:50:04 +0200

    home work completed

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function plus(num1, num2) {
 }
 function minus(num1, num2) {
     let result = num1 - num2
-    console.log(result)
+    console.log(result)         
 }
 function multiply(num1, num2) {
     let result = num1 * num2
@@ -37,9 +37,52 @@ const taskFive = 5
 console.log(`Task ${taskFive}`)
 
 function func1(n) {
-    for(let i = 1; i >= n; i++) {
-        console.log(n)
+    for(let i = 1; i <= n; i++) {
+        console.log(i)
+    }
+}
+func1(5)
+
+const taskSix = 6
+console.log(`Task ${taskSix}`)
+
+function func2(n) {
+    for(let i = 5; i >= n; i--) {
+        console.log(i)
+    }
+}
+func2(1)
+
+const taskSeven = 7
+console.log(`Task ${taskSeven}`)
+
+function pow(number, pow) {
+    let result = Math.pow(number, pow)
+    console.log(result)
+}
+pow(7,2)
+
+const taskEight = 8
+console.log(`Task ${taskEight}`)
+
+function isBigger(num1, num2) {
+    if(num1 > num2) {
+        console.log(true)
+    } else {
+        console.log(false)
     }
 }
 
-func1(5)
+isBigger(5, -1)
+
+const taskNine = 9
+console.log(`Task ${taskNine}`)
+
+function isSmaller(num1, num2) {
+    if(num1 < num2) {
+        console.log(true)
+    } else {
+        console.log(false)
+    }
+}
+isSmaller(5,-1)
